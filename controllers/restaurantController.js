081f9d9f0809fefd1a80b9587ce0f9f6c3dba1af
Jason S <ctediz@gmail.com>
2017-09-24 11:46:15 -0600

    Clean up for restaurantRoutes
    Moved logic to restaurantController.js
    Added check for malformed id

---
+++ controllers/restaurantController.js
@@ -0,0 +1,91 @@
+var restaurantController = function(Restaurant) {
+    // Base Route
+    var post = function(req, res) {
+        var restaurant = new Restaurant(req.body);
+        restaurant.save();
+        restaurant.status(201);
+        res.json(restaurant);
+    };
+
+    var getAll = function(req, res) {
+        var query = {};
+        Restaurant.find(query, function(err, restaurants) {
+            if(err) {
+                console.log(err);
+                res.status(500).send(err);
+            }
+            else {
+                res.json(restaurants);
+            }
+        });
+    };
+
+    // Single Routes
+    var put = function(req, res) {
+        var restaurant = require('../models/restaurantModel');
+        // loop over properties
+        restaurant.schema.eachPath(function(path) {
+            if(path != '_id' && path != '__v') {
+                req.restaurant[path] = req.body[path];
+            }
+        });
+
+        req.restaurant.save(function(err) {
+            if(err) {
+                console.log(err);
+                res.status(500).send(err);
+            }
+            else {
+                res.json(req.restaurant);
+            }
+        });
+    };
+
+    var patch = function(req, res) {
+        if(req.body._id) {
+            delete req.body._id;
+        }
+        if(req.body.__v) {
+            delete req.body.__v;
+        }
+
+        for(var p in req.body) {
+            req.restaurant[p] = req.body[p];
+        }
+
+        req.restaurant.save(function(err) {
+            if(err) {
+                console.log(err);
+                res.status(500).send(err);
+            }
+            else {
+                res.json(req.restaurant);
+            }
+        });
+    };
+
+    var deleteRestaurant = function(req, res) {
+        Restaurant.findById(req.params.id, function(err, restaurant) {
+            req.book.remove(function(err) {
+                if(err) {
+                    // Server error
+                    console.log(err);
+                    res.status(500).send('Something bad happened!');
+                }
+                else {
+                    res.status(204).send('Removed');
+                }
+            });
+        });
+    };
+
+    return {
+        post: post,
+        getAll: getAll,
+        put: put,
+        patch: patch,
+        delete: deleteRestaurant
+    };
+};
+
+module.exports = restaurantController;
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -1,106 +1,42 @@
 var express = require('express');
-var mongoose = require('mongoose');
 
 var router = function(Restaurant) {
     var resRouter = express.Router();
+    var restaurantController = require('../controllers/restaurantController')(Restaurant);
     
     resRouter.route('/')
-        .get(function(req, res) {
-            var query = {};
-            Restaurant.find(query, function(err, restaurants) {
+        .get(restaurantController.getAll)
+        .post(restaurantController.post);
+
+    // find book and pass on if found
+    resRouter.use('/:id', function(req, res, next) {
+        var mongoose = require('mongoose');
+        if(mongoose.Types.ObjectId.isValid(req.params.id)) {
+            Restaurant.findById(req.params.id ,function(err, restaurant) {
                 if(err) {
                     console.log(err);
                     res.status(500).send(err);
                 }
+                else if(restaurant) {
+                    req.restaurant = restaurant;
+                    next();
+                }
                 else {
-                    res.json(restaurants);
+                    req.status(404).send("Not found");
                 }
             });
-        })
-        .post(function(req, res) {
-            var restaurant = new Restaurant(req.body);
-            restaurant.save();
-            restaurant.status(201);
-            res.json(restaurant);
-        });
-
-
-    // find book and pass on if found
-    resRouter.use('/:id', function(req, res, next) {
-        // TODO: check for valid object ID
-        Restaurant.findById(req.params.id ,function(err, restaurant) {
-            if(err) {
-                console.log(err);
-                res.status(500).send(err);
-            }
-            else if(restaurant) {
-                req.restaurant = restaurant;
-                next();
-            }
-            else {
-                req.status(404).send("Not found");
-            }
-        });
+        }
+        else {
+            res.status(404).send('Malformed ID');
+        }
     });
     resRouter.route('/:id')
         .get(function(req, res) {
             res.json(req.restaurant);
         })
-        .put(function(req, res) {
-            var restaurant = require('../models/restaurantModel');
-            // loop over properties
-            restaurant.schema.eachPath(function(path) {
-                if(path != '_id' && path != '__v') {
-                    req.restaurant[path] = req.body[path];
-                }
-            });
-
-            req.restaurant.save(function(err) {
-                if(err) {
-                    console.log(err);
-                    res.status(500).send(err);
-                }
-                else {
-                    res.json(req.restaurant);
-                }
-            });
-        })
-        .patch(function(req, res) {
-            if(req.body._id) {
-                delete req.body._id;
-            }
-            if(req.body.__v) {
-                delete req.body.__v;
-            }
-
-            for(var p in req.body) {
-                req.restaurant[p] = req.body[p];
-            }
-
-            req.restaurant.save(function(err) {
-                if(err) {
-                    console.log(err);
-                    res.status(500).send(err);
-                }
-                else {
-                    res.json(req.restaurant);
-                }
-            });
-        })
-        .delete(function(req, res) {
-            Restaurant.findById(req.params.id, function(err, restaurant) {
-                req.book.remove(function(err) {
-                    if(err) {
-                        // Server error
-                        console.log(err);
-                        res.status(500).send('Something bad happened!');
-                    }
-                    else {
-                        res.status(204).send('Removed');
-                    }
-                });
-            });
-        });
+        .put(restaurantController.put)
+        .patch(restaurantController.patch)
+        .delete(restaurantController.delete);
 
         return resRouter;
 };
